87e9ff395f13912788e99b3e00d0f9098a033b52
Tychele Turner <tycheleturner@gmail.com>
2017-07-21 11:46:57 -0700

    [annotation] Added the get baits script

---
+++ annotation/get_baits.sh
@@ -0,0 +1,2 @@
+wget ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/AshkenazimTrio/analysis/OsloUniversityHospital_Exome_GATK_jointVC_11242015/wex_Agilent_SureSelect_v05_b37.baits.slop50.merged.list
+
